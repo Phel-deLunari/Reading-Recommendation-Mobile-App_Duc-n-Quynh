fcf54ed7012e606486c1140dd7b5408ad562750e
Nguyễn Minh Đức <ducnm.0405@gmail.com>
2025-11-09 20:07:26 +0700

    Loại bớt phần dư

--- main.dart
+++ main.dart
@@ -1,14 +1,12 @@
-// Toàn bộ code cho file: lib/main.dart
 
 import 'package:flutter/material.dart';
-import 'screens/main_screen.dart'; // <-- Gọi file MainScreen của CHÚNG TA
-import 'utils/localization.dart'; // <-- Gọi file ngôn ngữ
+import 'screens/main_screen.dart'; 
+import 'utils/localization.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-// Lấy MyApp từ code của bạn bạn
 class MyApp extends StatefulWidget {
   const MyApp({super.key});
 
@@ -33,9 +31,8 @@ class _MyAppState extends State<MyApp> {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
         useMaterial3: true,
       ),
-      // KHÔNG dùng 'man_hinh_chinh', mà dùng 'MainScreen' của chúng ta
-      // và truyền state ngôn ngữ vào
       home: MainScreen(ngonNgu: ngonNguHienTai, doiNgonNgu: doiNgonNgu),
     );
   }
 }
+
